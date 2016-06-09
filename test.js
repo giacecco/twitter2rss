46d167c20c21c7b7c0079280dcdf91f4b9ddcd8d
Gianfranco Cecconi <giacecco@dico.im>
2016-06-09 15:44:55 +0100

    Added getLists

--- test.js
+++ test.js
@@ -5,6 +5,14 @@ const exec = require('child_process').exec,
 
 // This maps t's own choice of columns vs mine
 const T_FIELD_MAPPING = {
+    "list": {
+        'from': [ 'ID', 'Created at', 'Screen name', 'Slug', 'Members',
+            'Subscribers', 'Mode', 'Description' ],
+        'to': [ 'id', 'createdAt', 'screenName', 'slug', 'noOfMembers',
+            'noOfSubscribers', 'mode', 'description' ],
+        'ints': [ 'noOfMembers', 'noOfSubscribers' ],
+        'dates': [ 'createdAt' ]
+    },
     "user": {
         'from': [ 'ID', 'Since', 'Last tweeted at', 'Tweets', 'Favorites',
             'Listed', 'Following', 'Followers', 'Screen name', 'Name',
@@ -21,11 +29,12 @@ const T_FIELD_MAPPING = {
 };
 
 const tFieldMap = function (mapType, entry) {
-    var newEntry = { };
+    var newEntry = JSON.parse(JSON.stringify(entry));
     // copies the attributes, changing their names
     var map = _.object(T_FIELD_MAPPING[mapType].from, T_FIELD_MAPPING[mapType].to);
     _.keys(map).forEach(function (fieldName) {
         newEntry[map[fieldName]] = entry[fieldName];
+        delete newEntry[fieldName];
     });
     // converts the integer values to int
     (T_FIELD_MAPPING[mapType].ints || [ ]).forEach(function (fieldName) {
@@ -42,6 +51,8 @@ const tFieldMap = function (mapType, entry) {
     return(newEntry);
 }
 
+// runs _t_ with the specified parameters and adds a transformation of type
+// _mapType_ to each entry
 const execT = function(parameters, mapType, callback) {
     exec('t ' + parameters, (error, stdout, stderr) => {
         if (error) {
@@ -57,11 +68,11 @@ const execT = function(parameters, mapType, callback) {
     });
 }
 
-const getListMembers = function (slug, callback) { execT('list members -l --csv ' + slug, "user", callback); }
+const getListMembers = function (slug, callback) { execT('list members -l --csv ' + slug, 'user', callback); }
 
 const getLists = function (callback) {
-    execT('lists -l --csv', function (err, lists) {
-        async.each(lists, function (err, list) {
+    execT('lists -l --csv', 'list', function (err, lists) {
+        async.each(lists, function (list, callback) {
             getListMembers(list.slug, function (err, listMembers) {
                 list.members = listMembers;
                 callback(null);
@@ -72,6 +83,6 @@ const getLists = function (callback) {
     });
 }
 
-getListMembers('tech4good', function (err, data) {
-    console.log(data);
+getLists(function (err, data) {
+    console.log(JSON.stringify(data));
 });
