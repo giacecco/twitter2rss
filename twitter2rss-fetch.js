e4af967794b87c274d323349970427632c484062
Gianfranco Cecconi <giacecco@dico.im>
2017-01-27 10:58:16 +0000

    Added support for both configuration files and command line arguments

--- twitter2rss-fetch.js
+++ twitter2rss-fetch.js
@@ -7,7 +7,7 @@ const async = require("async"),
       _ = require("underscore"),
       argv = require('yargs')
           .usage("Usage: $0 \
-              [--debug path_to_feed_configuration_file] \
+              [configuration_file] \
               [--retweets] \
               [--replies] \
               [--noise] \
@@ -44,108 +44,113 @@ var twitter = new T2({
   "tokensecret": argv.tokensecret ? argv.tokensecret : process.env.TWITTER2RSS_ACCESS_TOKEN_SECRET
 });
 
-var configuration = argv.debug;
-fs.readFile(configuration, { "encoding": "utf8" }, (err, text) => {
-    if (err) {
-        console.error("Error reading configuration file " + configurationFile + " with error " + err.message);
-        system.exit(1);
+let configuration = { "searches": [ ], "lists": [ ] };
+// interprets all specified configuration files
+argv._.forEach(configurationFile => {
+    if (fileExistsSync(configurationFile)) {
+        let c = JSON.parse(fs.readFileSync(configurationFile, { "encoding": "utf8" }));
+        configuration.searches = _.uniq(configuration.searches.concat(c.searches));
+        configuration.lists = _.uniq(configuration.lists.concat(c.lists));
     }
-    configuration = JSON.parse(text);
-    var results = [ ];
-    async.parallel([
-        callback => {
-            // all the searches
-            async.map(configuration.searches, (searchString, callback) => {
-                async.map(argv.language, (lang, callback) => {
-                    twitter.getSearchTweets({
-                        "q": searchString,
-                        "lang": lang, //search/tweets allows me to specify a language
-                        "count": 100,
-                        "resultType": "recent"
-                    }, (err, results) => {
-                        // TODO: manage error here
-                        callback(null, results.statuses);
-                    });
-                }, (err, r) => {
-                    callback(err, _.flatten(r, true));
+});
+// adds anything specified directly on the command line
+configuration.searches = argv.search ? configuration.searches.concat(argv.search) : configuration.searches;
+configuration.lists = argv.list ? configuration.lists.concat(argv.list) : configuration.lists;
+// does the job
+let results = [ ];
+async.parallel([
+    callback => {
+        // all the searches
+        async.map(configuration.searches, (searchString, callback) => {
+            async.map(argv.language, (lang, callback) => {
+                twitter.getSearchTweets({
+                    "q": searchString,
+                    "lang": lang, //search/tweets allows me to specify a language
+                    "count": 100,
+                    "resultType": "recent"
+                }, (err, results) => {
+                    // TODO: manage error here
+                    callback(null, results.statuses);
+                });
+            }, (err, r) => {
+                callback(err, _.flatten(r, true));
+            });
+        }, (err, r) => {
+            callback(err, results = results.concat(_.flatten(r, true)));
+        });
+    },
+    callback => {
+        // all the lists
+        twitter.getListsList((err, lists) => {
+            // TODO: manage error here
+            lists = lists.reduce((memo, list) => memo.concat(_.contains(configuration.lists, list.name) ? list.id_str : [ ]), [ ]);
+            async.map(lists, (listId, callback) => {
+                twitter.getListsStatuses({
+                    "list_id": listId,
+                    "count": 100 // not clear if there's a max here
+                }, (err, results) => {
+                    // TODO: manage error here
+                    // NOTE: the tweets' language cannot be specified in lists/statuses ,
+                    //       hence the filtering here
+                    callback(null, results);
                 });
             }, (err, r) => {
                 callback(err, results = results.concat(_.flatten(r, true)));
             });
-        },
-        callback => {
-            // all the lists
-            twitter.getListsList((err, lists) => {
-                // TODO: manage error here
-                lists = lists.reduce((memo, list) => memo.concat(_.contains(configuration.lists, list.name) ? list.id_str : [ ]), [ ]);
-                async.map(lists, (listId, callback) => {
-                    twitter.getListsStatuses({
-                        "list_id": listId,
-                        "count": 100 // not clear if there's a max here
-                    }, (err, results) => {
-                        // TODO: manage error here
-                        // NOTE: the tweets' language cannot be specified in lists/statuses ,
-                        //       hence the filtering here
-                        callback(null, results);
-                    });
-                }, (err, r) => {
-                    callback(err, results = results.concat(_.flatten(r, true)));
-                });
-            });
-        }
-    ], err => {
-
-        // delete duplicates coming from the same tweet being captured by
-        // different searches and lists, identified by tweet id
-        results = _.uniq(results, r => r.id_str);
-
-        // drop retweets, checks both the metadata and the text
-        if (argv.retweets) results = results.filter(s => !s.in_reply_to_status_id_str && !s.text.match(/^rt /i));
-
-        // drop replies, checks both the metadata and the text
-        if (argv.retweets) results = results.filter(s => !s.in_reply_to_user_id_str && !s.text.match(/^@/));
-
-        // sort in chronological order
-        results.forEach(s => s.created_at = new Date(s.created_at));
-        results = results.sort((x, y) => x.created_at - y.created_at);
-
-        // drops messages that differ just by the hashtags or URLs they
-        // reference and keep the oldest tweet only
-        if (argv.noise) {
-            results = _.uniq(results, s => s.text
-                // drop the URLs
-                .replace(URL_REGEX, "")
-                // drop the hashtags
-                .replace(/#[\w-]+/g, "")
-                // drop all dirty characters and spaces
-                .replace(/[^A-Za-z0-9]/g, "")
-            );
-        }
-
-        // final touches
-        results = results
-            // filter for the required languages
-            .filter(s => _.contains(argv.language, s.lang));
-
-        // --post directives and output
-        // NOTE: this is the same code as in t2cli.json in
-        //       Digital-Contraptions-Imaginarium/t2
-        async.reduce(!argv.post ? [ "x => JSON.stringify(x)" ] : [ ].concat(argv.post), results, (memo, p, callback) => {
-            p = eval(fileExistsSync(p) ? fs.readFileSync(p, { "encoding": "utf8" }) : p);
-            if (p.length > 1) {
-                // the --post function is asynchronous
-                return p(memo, callback);
-            } else {
-                // the --post function is synchronous
-                callback(null, p(memo));
-            }
-        }, (err, results) => {
-            if (err) {
-                console.error("Undefined error in executing the --post commands.");
-                process.exit(1);
-            }
-            console.log(results);
         });
+    }
+], err => {
 
+    // delete duplicates coming from the same tweet being captured by
+    // different searches and lists, identified by tweet id
+    results = _.uniq(results, r => r.id_str);
+
+    // drop retweets, checks both the metadata and the text
+    if (argv.retweets) results = results.filter(s => !s.in_reply_to_status_id_str && !s.text.match(/^rt /i));
+
+    // drop replies, checks both the metadata and the text
+    if (argv.retweets) results = results.filter(s => !s.in_reply_to_user_id_str && !s.text.match(/^@/));
+
+    // sort in chronological order
+    results.forEach(s => s.created_at = new Date(s.created_at));
+    results = results.sort((x, y) => x.created_at - y.created_at);
+
+    // drops messages that differ just by the hashtags or URLs they
+    // reference and keep the oldest tweet only
+    if (argv.noise) {
+        results = _.uniq(results, s => s.text
+            // drop the URLs
+            .replace(URL_REGEX, "")
+            // drop the hashtags
+            .replace(/#[\w-]+/g, "")
+            // drop all dirty characters and spaces
+            .replace(/[^A-Za-z0-9]/g, "")
+        );
+    }
+
+    // final touches
+    results = results
+        // filter for the required languages
+        .filter(s => _.contains(argv.language, s.lang));
+
+    // --post directives and output
+    // NOTE: this is the same code as in t2cli.json in
+    //       Digital-Contraptions-Imaginarium/t2
+    async.reduce(!argv.post ? [ "x => JSON.stringify(x)" ] : [ ].concat(argv.post), results, (memo, p, callback) => {
+        p = eval(fileExistsSync(p) ? fs.readFileSync(p, { "encoding": "utf8" }) : p);
+        if (p.length > 1) {
+            // the --post function is asynchronous
+            return p(memo, callback);
+        } else {
+            // the --post function is synchronous
+            callback(null, p(memo));
+        }
+    }, (err, results) => {
+        if (err) {
+            console.error("Undefined error in executing the --post commands.");
+            process.exit(1);
+        }
+        console.log(results);
     });
+
 });
