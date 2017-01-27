bd5daeb8f358b957b1bb0e1e009d8855f1c82fbb
Gianfranco Cecconi <giacecco@dico.im>
2017-01-27 13:55:19 +0000

    Fixed wrong SQL escaping

--- utils/insertIntoSqlite3.js
+++ utils/insertIntoSqlite3.js
@@ -1,1 +1,1 @@
-r => r.map(x => "INSERT OR IGNORE INTO tweets (id, payload) VALUES ('" + x.id_str + "', '" + JSON.stringify(x).replace("'", "''") + "');").join("\n")
+r => r.map(x => "INSERT OR IGNORE INTO tweets (id, payload) VALUES ('" + x.id_str + "', '" + JSON.stringify(x).replace(/'/g, "''") + "');").join("\n")
