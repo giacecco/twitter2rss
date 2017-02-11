2fb2560ab984500fb19e685bf14014beb754079e
Gianfranco Cecconi <giacecco@dico.im>
2017-02-11 18:37:39 +0000

    Fixes bug that ignored the tweets' language

--- utils/insertIntoMariadb.sql
+++ utils/insertIntoMariadb.sql
@@ -1,12 +1,16 @@
 CREATE TABLE IF NOT EXISTS tweets (
-    id_str VARCHAR(18) UNIQUE,
+    id_str VARCHAR(18),
     created_at DATETIME,
     user_id_str VARCHAR(18),
     user_name VARCHAR(20) CHARACTER SET utf8,
     user_screen_name VARCHAR(15) CHARACTER SET utf8,
-    text VARCHAR(150) CHARACTER SET utf8
+    text VARCHAR(150) CHARACTER SET utf8,
+    lang VARCHAR(2),
+    PRIMARY KEY (id_str)
 );
+
 CREATE TABLE IF NOT EXISTS tweets_themes (
     id_str VARCHAR(18) UNIQUE,
-    theme VARCHAR(150) CHARACTER SET utf8
+    theme VARCHAR(150) CHARACTER SET utf8,
+    PRIMARY KEY (id_str, theme)
 );
